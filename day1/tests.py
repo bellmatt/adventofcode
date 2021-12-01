1330314f8dab06acb89086f98423b12ff7e14455
Matt Bell <38588937+bellmatt@users.noreply.github.com>
2021-12-01 13:58:11 +0000

    Day 1

---
+++ day1/tests.py
@@ -0,0 +1,17 @@
+from day1 import get_sliding_window_measurements, day1part1,day1part2
+
+
+def test_sum_depths():
+    input = [199,200,208,210,200,207,240,269,260,263]
+    expected = [607,618,618,617,647,716,769,792]
+    assert expected == get_sliding_window_measurements(input)
+
+def test_increase_counts_part1():
+    input = [199,200,208,210,200,207,240,269,260,263]
+    expected = 7
+    assert expected == day1part1(input)
+
+def test_increase_counts_part2():
+    input = [199,200,208,210,200,207,240,269,260,263]
+    expected = 5
+    assert expected == day1part2(input)
