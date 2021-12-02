21e920c7a5d1cd57e53bab1c021d1b6765417086
Matt Bell <38588937+bellmatt@users.noreply.github.com>
2021-12-02 12:10:28 +0000

    Refactor to use dataclasses

--- src/day2.py
+++ src/day2.py
@@ -1,46 +1,69 @@
-from typing import Tuple
+from dataclasses import dataclass
 
 
-def recalculate_position(current: Tuple[int, int], instruction: str) -> Tuple[int, int]:
-    """Returns an updated position (horizontal position, depth) given an instruction"""
-    direction, distance = instruction.split(" ")
-    if direction == "forward":
-        return (current[0] + int(distance), current[1])
-    if direction == "up":
-        return (current[0], current[1] - int(distance))
-    if direction == "down":
-        return (current[0], current[1] + int(distance))
+@dataclass
+class Position:
+    horizontal: int = 0
+    depth: int = 0
+    aim: int = 0
+
+    def __init__(self, horizontal: int = 0, depth: int = 0, aim: int = 0):
+        self.horizontal = horizontal
+        self.depth = depth
+        self.aim = aim
+
+
+@dataclass
+class Instruction:
+    direction: str
+    distance: int
+
+    def __init__(self, input: str):
+        self.direction, dist = input.split(" ")
+        self.distance = int(dist)
+
+
+def recalculate_position(position: Position, instruction_input: str) -> Position:
+    """Returns an updated position after executing an instruction"""
+    instruction = Instruction(instruction_input)
+    if instruction.direction == "forward":
+        position.horizontal = position.horizontal + instruction.distance
+    if instruction.direction == "up":
+        position.depth = position.depth - instruction.distance
+    if instruction.direction == "down":
+        position.depth = position.depth + instruction.distance
+    return position
 
 
 def recalculate_position_with_aim(
-    current: Tuple[int, int, int], instruction: str
-) -> Tuple[int, int, int]:
-    """Returns an updated position (horizontal position, aim, depth) given an instruction"""
-    direction, distance = instruction.split(" ")
-    new_position = current
-    if direction == "forward":
-        new_position = (
-            current[0] + int(distance),
-            current[1],
-            current[2] + current[1] * int(distance),
-        )
-        return new_position
-    if direction == "up":
-        return (current[0], current[1] - int(distance), current[2])
-    if direction == "down":
-        return (current[0], current[1] + int(distance), current[2])
+    position: Position, instruction_input: str
+) -> Position:
+    """Returns an updated position after executing an instruction"""
+    instruction = Instruction(instruction_input)
+    if instruction.direction == "forward":
+        position.horizontal = position.horizontal + instruction.distance
+        position.depth = position.depth + (position.aim * instruction.distance)
+    if instruction.direction == "up":
+        position.aim = position.aim - instruction.distance
+    if instruction.direction == "down":
+        position.aim = position.aim + instruction.distance
+    return position
 
 
 if __name__ == "__main__":
     lines = open("./src/day2_input.txt", "r").readlines()
-    current_position = (0, 0)
-    current_position_with_aim = (0, 0, 0)
+    current_position = Position()
+    current_position_with_aim = Position()
     for input in lines:
         current_position = recalculate_position(current_position, input)
         current_position_with_aim = recalculate_position_with_aim(
             current_position_with_aim, input
         )
-    print(current_position)
-    print(current_position[0] * current_position[1])
-    print(current_position_with_aim)
-    print(current_position_with_aim[0] * current_position_with_aim[2])
+    print("part 1:")
+    print(f"h: {current_position.horizontal} d: {current_position.depth} ")
+    print(current_position.horizontal * current_position.depth)
+    print("part 2:")
+    print(
+        f"h: {current_position_with_aim.horizontal} d: {current_position_with_aim.depth} a: {current_position_with_aim.aim}"
+    )
+    print(current_position_with_aim.horizontal * current_position_with_aim.depth)
--- src/test_day2.py
+++ src/test_day2.py
@@ -4,30 +4,42 @@ import pytest
 
 
 @pytest.mark.parametrize(
-    "current,input,expected",
+    "cur_h, cur_d, instruction,exp_h, exp_d",
     [
-        ((0, 0), "forward 5", (5, 0)),
-        ((5, 0), "down 5", (5, 5)),
-        ((5, 5), "forward 8", (13, 5)),
-        ((13, 5), "up 3", (13, 2)),
-        ((13, 2), "down 8", (13, 10)),
-        ((13, 10), "forward 2", (15, 10)),
+        (0, 0, "forward 5", 5, 0),
+        (5, 0, "down 5", 5, 5),
+        (5, 5, "forward 8", 13, 5),
+        (13, 5, "up 3", 13, 2),
+        (13, 2, "down 8", 13, 10),
+        (13, 10, "forward 2", 15, 10),
     ],
 )
-def test_position_change(current, input, expected) -> List:
-    assert expected == day2.recalculate_position(current, input)
+def test_position_change(cur_h, cur_d, instruction, exp_h, exp_d):
+    current_pos = day2.Position(cur_h, cur_d)
+    assert exp_h == day2.recalculate_position(current_pos, instruction).horizontal
+    current_pos = day2.Position(cur_h, cur_d)
+    assert exp_d == day2.recalculate_position(current_pos, instruction).depth
 
 
 @pytest.mark.parametrize(
-    "current,input,expected",
+    "cur_h, cur_a, cur_d,instruction,exp_h,exp_a,exp_d",
     [
-        ((0, 0, 0), "forward 5", (5, 0, 0)),
-        ((5, 0, 0), "down 5", (5, 5, 0)),
-        ((5, 5, 0), "forward 8", (13, 5, 40)),
-        ((13, 5, 40), "up 3", (13, 2, 40)),
-        ((13, 2, 40), "down 8", (13, 10, 40)),
-        ((13, 10, 40), "forward 2", (15, 10, 60)),
+        (0, 0, 0, "forward 5", 5, 0, 0),
+        (5, 0, 0, "down 5", 5, 5, 0),
+        (5, 5, 0, "forward 8", 13, 5, 40),
+        (13, 5, 40, "up 3", 13, 2, 40),
+        (13, 2, 40, "down 8", 13, 10, 40),
+        (13, 10, 40, "forward 2", 15, 10, 60),
     ],
 )
-def test_position_change_with_aim(current, input, expected) -> List:
-    assert expected == day2.recalculate_position_with_aim(current, input)
+def test_position_change_with_aim(
+    cur_h, cur_a, cur_d, instruction, exp_h, exp_a, exp_d
+) -> List:
+    current_pos = day2.Position(cur_h, cur_d, cur_a)
+    assert (
+        exp_h == day2.recalculate_position_with_aim(current_pos, instruction).horizontal
+    )
+    current_pos = day2.Position(cur_h, cur_d, cur_a)
+    assert exp_d == day2.recalculate_position_with_aim(current_pos, instruction).depth
+    current_pos = day2.Position(cur_h, cur_d, cur_a)
+    assert exp_a == day2.recalculate_position_with_aim(current_pos, instruction).aim
