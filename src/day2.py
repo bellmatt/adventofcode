8760d1e818a3ab309b543e5f59ebf19828243331
Matt Bell <38588937+bellmatt@users.noreply.github.com>
2021-12-02 11:23:48 +0000

    Day 2 part 2

--- src/day2.py
+++ src/day2.py
@@ -2,6 +2,7 @@ from typing import Tuple
 
 
 def recalculate_position(current: Tuple[int, int], instruction: str) -> Tuple[int, int]:
+    """Returns an updated position (horizontal position, depth) given an instruction"""
     direction, distance = instruction.split(" ")
     if direction == "forward":
         return (current[0] + int(distance), current[1])
@@ -11,10 +12,35 @@ def recalculate_position(current: Tuple[int, int], instruction: str) -> Tuple[in
         return (current[0], current[1] + int(distance))
 
 
+def recalculate_position_with_aim(
+    current: Tuple[int, int, int], instruction: str
+) -> Tuple[int, int, int]:
+    """Returns an updated position (horizontal position, aim, depth) given an instruction"""
+    direction, distance = instruction.split(" ")
+    new_position = current
+    if direction == "forward":
+        new_position = (
+            current[0] + int(distance),
+            current[1],
+            current[2] + current[1] * int(distance),
+        )
+        return new_position
+    if direction == "up":
+        return (current[0], current[1] - int(distance), current[2])
+    if direction == "down":
+        return (current[0], current[1] + int(distance), current[2])
+
+
 if __name__ == "__main__":
     lines = open("./src/day2_input.txt", "r").readlines()
     current_position = (0, 0)
+    current_position_with_aim = (0, 0, 0)
     for input in lines:
         current_position = recalculate_position(current_position, input)
+        current_position_with_aim = recalculate_position_with_aim(
+            current_position_with_aim, input
+        )
     print(current_position)
     print(current_position[0] * current_position[1])
+    print(current_position_with_aim)
+    print(current_position_with_aim[0] * current_position_with_aim[2])
--- src/test_day2.py
+++ src/test_day2.py
@@ -16,3 +16,18 @@ import pytest
 )
 def test_position_change(current, input, expected) -> List:
     assert expected == day2.recalculate_position(current, input)
+
+
+@pytest.mark.parametrize(
+    "current,input,expected",
+    [
+        ((0, 0, 0), "forward 5", (5, 0, 0)),
+        ((5, 0, 0), "down 5", (5, 5, 0)),
+        ((5, 5, 0), "forward 8", (13, 5, 40)),
+        ((13, 5, 40), "up 3", (13, 2, 40)),
+        ((13, 2, 40), "down 8", (13, 10, 40)),
+        ((13, 10, 40), "forward 2", (15, 10, 60)),
+    ],
+)
+def test_position_change_with_aim(current, input, expected) -> List:
+    assert expected == day2.recalculate_position_with_aim(current, input)
