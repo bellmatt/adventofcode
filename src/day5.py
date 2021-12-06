5f8c4cbd27673f4d51d9debff2ff3a7017cd7ce3
Matt Bell <38588937+bellmatt@users.noreply.github.com>
2021-12-06 11:54:11 +0000

    Day 5 part 2

--- src/day5.py
+++ src/day5.py
@@ -20,18 +20,41 @@ class VentPosition:
         self.positions = []
         if self.line_start.x == self.line_end.x:
             if self.line_start.y < self.line_end.y:
+                # (1,1) -> (1,3)
                 for y in range(self.line_start.y, self.line_end.y + 1, 1):
                     self.positions.append(Position(self.line_start.x, y))
             else:
+                # (1,3) -> (1,1)
                 for y in range(self.line_end.y, self.line_start.y + 1, 1):
                     self.positions.append(Position(self.line_start.x, y))
         elif self.line_start.y == self.line_end.y:
             if self.line_start.x < self.line_end.x:
+                # (1,1) -> (3,1)
                 for x in range(self.line_start.x, self.line_end.x + 1, 1):
                     self.positions.append(Position(x, self.line_start.y))
             else:
+                # (3,1) -> (1,1)
                 for x in range(self.line_end.x, self.line_start.x + 1, 1):
                     self.positions.append(Position(x, self.line_start.y))
+        # Diagonals
+        elif self.line_start.x < self.line_end.x:
+            if self.line_start.y < self.line_end.y:
+                # (1,1) -> (3,3)
+                for i, x in enumerate(range(self.line_start.x, self.line_end.x + 1, 1)):
+                    self.positions.append(Position(self.line_start.x+i, self.line_start.y+i))
+            else:
+                # (1,3) -> (3,1)
+                for i, x in enumerate(range(self.line_start.x, self.line_end.x + 1, 1)):
+                    self.positions.append(Position(self.line_start.x+i, self.line_start.y-i))
+        elif self.line_start.x > self.line_end.x:
+            if self.line_start.y < self.line_end.y:
+                # (3,1) -> (1,3)
+                for i, x in enumerate(range(self.line_end.x, self.line_start.x + 1, 1)):
+                    self.positions.append(Position(self.line_start.x-i, self.line_start.y+i))
+            else:
+                # (3,3) -> (1,1)
+                for i, x in enumerate(range(self.line_end.x, self.line_start.x + 1, 1)):
+                    self.positions.append(Position(self.line_start.x-i, self.line_start.y-i))
 
 
 def parse_input(input: List[str]):
