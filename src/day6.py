c2c09d0ccebb2617f837e33b4aca8862afc23bba
Matt Bell <38588937+bellmatt@users.noreply.github.com>
2021-12-06 16:56:38 +0000

    day 6 part 2

--- src/day6.py
+++ src/day6.py
@@ -1,10 +1,28 @@
 from typing import List
 
 
-def simulate_fish(fishes: List[str], days=0) -> int:
+def simulate_fish_v2(fishes: List[int], days=0) -> int:
+    """Put fish into buckets depending on what day we need to update them"""
+    buckets = [0] * (days + 9)
+    for f in fishes:
+        buckets[f] += 1
+    for day in range(days):
+        while buckets[day] > 0:
+            # Reset fish timer
+            buckets[day + 7] += buckets[day]
+            # Add new fish
+            buckets[day + 9] += buckets[day]
+            # Reset the bucket for this day
+            buckets[day] = 0
+    return sum(buckets)
+
+
+def simulate_fish(fishes: List[int], days=0) -> int:
+    """loop over the list and process TTL of every fish"""
     day = 0
+
     while day < days:
-        # print(f"After {day} days:  {fishes}")
+        fishes = sorted(fishes)
         new_fish = []
         for i, fish in enumerate(fishes):
             if fish == 0:
@@ -20,7 +38,9 @@ def simulate_fish(fishes: List[str], days=0) -> int:
 
 if __name__ == "__main__":
     fish_internal_timers = list(
-        int(x.strip())
-        for x in open("./src/day6_testinput.txt", "r").readline().split(",")
+        int(x.strip()) for x in open("./src/day6_input.txt", "r").readline().split(",")
     )
+    # Part 1
     print(simulate_fish(fish_internal_timers, 80))
+    # Part 2
+    print(simulate_fish_v2(fish_internal_timers, 256))
