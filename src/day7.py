2f8775814e3e78723dc22a87442ca4d49f79deae
Matt Bell <38588937+bellmatt@users.noreply.github.com>
2021-12-10 16:29:11 +0000

    Day 7 part 2

--- src/day7.py
+++ src/day7.py
@@ -5,20 +5,22 @@ if __name__ == "__main__":
     crab_horizontal_positions = sorted(
         list(
             int(x.strip())
-            for x in open("./src/day7_testinput.txt", "r").readline().split(",")
+            for x in open("./src/day7_input.txt", "r").readline().split(",")
         )
     )
     target = int(statistics.median(crab_horizontal_positions))
     fuel_used = sum([abs(crab - target) for crab in crab_horizontal_positions])
     print(f"Part 1: {fuel_used}")
 
-    # This works for test data, but not the real data:
-    target = statistics.mean(crab_horizontal_positions)
-    fuel_used = sum(
-        [
-            (abs(crab - target) * (abs(crab - target) + 1)) / 2
-            for crab in crab_horizontal_positions
-        ]
-    )
+    fuel_used_per_position = []
+    for pos in range(min(crab_horizontal_positions), max(crab_horizontal_positions)):
+        fuel_used_per_position.append(
+            sum(
+                [
+                    (abs(crab - pos) * (abs(crab - pos) + 1)) / 2
+                    for crab in crab_horizontal_positions
+                ]
+            )
+        )
 
-    print(f"Part 2: {int(ceil(fuel_used))}")
+    print(f"Part 2: {min(fuel_used_per_position)}")
