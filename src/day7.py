4f728ff46d0348116c1ed02868f5014dca6341a8
Matt Bell <38588937+bellmatt@users.noreply.github.com>
2021-12-10 16:20:02 +0000

    day 7 refactor

--- src/day7.py
+++ src/day7.py
@@ -1,18 +1,24 @@
 import statistics
-
+from math import ceil
 
 if __name__ == "__main__":
     crab_horizontal_positions = sorted(
         list(
             int(x.strip())
-            for x in open("./src/day7_input.txt", "r").readline().split(",")
+            for x in open("./src/day7_testinput.txt", "r").readline().split(",")
         )
     )
     target = int(statistics.median(crab_horizontal_positions))
-    fuel_used = 0
-    for crab in crab_horizontal_positions:
-        if crab > target:
-            fuel_used += crab - target
-        else:
-            fuel_used += target - crab
-    print(fuel_used)
+    fuel_used = sum([abs(crab - target) for crab in crab_horizontal_positions])
+    print(f"Part 1: {fuel_used}")
+
+    # This works for test data, but not the real data:
+    target = statistics.mean(crab_horizontal_positions)
+    fuel_used = sum(
+        [
+            (abs(crab - target) * (abs(crab - target) + 1)) / 2
+            for crab in crab_horizontal_positions
+        ]
+    )
+
+    print(f"Part 2: {int(ceil(fuel_used))}")
