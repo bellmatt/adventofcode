73ecd5f1c1c72c2e6816d39dfd8322b71e71d843
Matt Bell <38588937+bellmatt@users.noreply.github.com>
2021-12-12 16:32:05 +0000

    refactor

--- src/day12.py
+++ src/day12.py
@@ -5,12 +5,18 @@ def is_small_cave(input: str) -> bool:
     return not input.isupper() and input not in ("start", "end")
 
 
-def find_next_route_options(curr_pos: str, input: List[Tuple[str, str]]) -> List[str]:
+def find_next_route_options(
+    curr_pos: str, input: List[Tuple[str, str]], path: List[str]
+) -> List[str]:
     options = []
     for route in input:
-        if route[0] == curr_pos:
+        if route[0] == curr_pos and not (is_small_cave(route[1]) and route[1] in path):
             options.append(route[1])
-        elif route[1] == curr_pos and route[0] not in ("start", "end"):
+        elif (
+            route[1] == curr_pos
+            and not (is_small_cave(route[0]) and route[0] in path)
+            and route[0] not in ("start", "end")
+        ):
             options.append(route[0])
     return options
 
@@ -21,7 +27,7 @@ if __name__ == "__main__":
         for line in open("./src/day12_testinput.txt", "r").readlines()
     ]
     curr_pos = "start"
-    paths = [["start"]]
+    paths = [[curr_pos]]
     finished_paths = 0
     while finished_paths < len(paths):
         for path in paths:
@@ -30,25 +36,15 @@ if __name__ == "__main__":
             if "end" not in path and "stuck" not in path:
                 curr_path_updated = False
                 # Find the possible routes from last entry in the path
-                options = find_next_route_options(current_loc, cave_system)
+                options = find_next_route_options(current_loc, cave_system, path)
                 for dest in options:
                     if not curr_path_updated:
-                        if is_small_cave(dest) and path.count(dest) < 1:
-                            # Small cave not visited before in this path
-                            path.append(dest)
-                            curr_path_updated = True
-                        elif dest.isupper() or dest == "end":
-                            path.append(dest)
-                            curr_path_updated = True
-                            if dest == "end":
-                                finished_paths += 1
+                        path.append(dest)
+                        curr_path_updated = True
+                        if dest == "end":
+                            finished_paths += 1
                     else:
-                        # Create new path
-                        if (
-                            (is_small_cave(dest) and dest not in path_copy)
-                            or dest.isupper()
-                            or dest == "end"
-                        ) and (path_copy + [dest]) not in paths:
+                        if (path_copy + [dest]) not in paths:
                             paths.append(path_copy + [dest])
                             curr_path_updated = True
                             if dest == "end":
