280f8f9e6b1553b4001efae25a6f8a555a14b306
Matt Bell <38588937+bellmatt@users.noreply.github.com>
2021-12-14 16:17:05 +0000

    Day 13 part 1

---
+++ src/day13.py
@@ -0,0 +1,75 @@
+from typing import List, Tuple
+
+
+def fold_over_y(input: List[List[str]], instruction: Tuple[str, int]) -> List[str]:
+    input[instruction[1]] = ["-"] * len(input[instruction[1]])
+    top_half = input[: instruction[1]]
+    bottom_half = input[instruction[1] + 1 :]
+    bottom_half.reverse()
+    input = top_half
+    for i, bottom in enumerate(bottom_half):
+        input[i] = [
+            "#" if x[0] == "#" or x[1] == "#" else "." for x in zip(input[i], bottom)
+        ]
+
+    return input
+
+
+def get_map_dimensions(input: List[Tuple[int, int]]) -> Tuple[int, int]:
+    size_x = 0
+    size_y = 0
+    for coordinate in input:
+        if coordinate[0] > size_x:
+            size_x = coordinate[0]
+        if coordinate[1] > size_y:
+            size_y = coordinate[1]
+    return (size_x + 1, size_y + 1)
+
+
+def plot_map(
+    input: List[Tuple[int, int]], dimensions: Tuple[int, int]
+) -> List[List[int]]:
+    transparent_paper_map = []
+    for y in range(dimensions[1]):
+        transparent_paper_map.append([""] * dimensions[0])
+    for y in range(len(transparent_paper_map)):
+        for x in range(len(transparent_paper_map[y])):
+            if (x, y) in input:
+                transparent_paper_map[y][x] = "#"
+            else:
+                transparent_paper_map[y][x] = "."
+    return transparent_paper_map
+
+
+def print_map(input: List[List[str]]) -> None:
+    for i in range(len(input)):
+        for j in range(len(input[i])):
+            print(input[i][j], end="")
+        print("")
+
+
+def count_dots(input: List[List[str]]) -> int:
+    count_of_dots = 0
+    for i in range(len(input)):
+        for j in range(len(input[i])):
+            if input[i][j] == "#":
+                count_of_dots += 1
+    return count_of_dots
+
+
+if __name__ == "__main__":
+    input = open("./src/day13_testinput.txt", "r").readlines()
+    dots = []
+    fold_instructions = []
+    for line in input:
+        line = line.rstrip()
+        if "," in line:
+            dots.append((int(line.split(",")[0]), int(line.split(",")[1])))
+        elif "fold" in line:
+            fold_instructions.append(
+                (line.split("=")[0].split()[2], int(line.split("=")[1]))
+            )
+    paper = plot_map(dots, get_map_dimensions(dots))
+    folded_paper = fold_over_y(paper, fold_instructions[0])
+
+    print(count_dots(folded_paper))
