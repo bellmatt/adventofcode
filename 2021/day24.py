3142f23ce1a1dbee4d705db9377da9cc39fbb310
Matt Bell <38588937+bellmatt@users.noreply.github.com>
2022-12-01 09:25:00 +0000

    deps update and lint

--- 2021/day24.py
+++ 2021/day24.py
@@ -2,14 +2,16 @@ from typing import List
 import sys
 from pathlib import PurePath
 
-def is_int(input:str):
+
+def is_int(input: str):
     try:
         return True, int(input)
     except ValueError:
         return False, input
 
+
 def process(instructions: List[str], input: List[int]) -> int:
-    variables = {"w":0,"x":0,"y":0,"z":0}
+    variables = {"w": 0, "x": 0, "y": 0, "z": 0}
 
     for instruction in instructions:
         print(instruction)
@@ -26,29 +28,38 @@ def process(instructions: List[str], input: List[int]) -> int:
             if instruction[0] == "mul":
                 variables[instruction[1]] = variables[instruction[1]] * value
             if instruction[0] == "eql":
-                variables[instruction[1]] = 1 if variables[instruction[1]] == value else 0
+                variables[instruction[1]] = (
+                    1 if variables[instruction[1]] == value else 0
+                )
         else:
             if instruction[0] == "add":
-                variables[instruction[1]] = variables[instruction[1]] + variables[instruction[2]]
+                variables[instruction[1]] = (
+                    variables[instruction[1]] + variables[instruction[2]]
+                )
             if instruction[0] == "mod":
-                variables[instruction[1]] = variables[instruction[1]] % variables[instruction[2]]
+                variables[instruction[1]] = (
+                    variables[instruction[1]] % variables[instruction[2]]
+                )
             if instruction[0] == "div":
-                variables[instruction[1]] = variables[instruction[1]] // variables[instruction[2]]
+                variables[instruction[1]] = (
+                    variables[instruction[1]] // variables[instruction[2]]
+                )
             if instruction[0] == "mul":
-                variables[instruction[1]] = variables[instruction[1]] * variables[instruction[2]]
+                variables[instruction[1]] = (
+                    variables[instruction[1]] * variables[instruction[2]]
+                )
             if instruction[0] == "eql":
-                variables[instruction[1]] = 1 if variables[instruction[1]] == variables[instruction[2]] else 0
+                variables[instruction[1]] = (
+                    1 if variables[instruction[1]] == variables[instruction[2]] else 0
+                )
 
         print(variables)
     return variables["z"]
 
 
-
-
 if __name__ == "__main__":
     # instructions = [ x.rstrip().split(" ") for x in open("./src/day24_input.txt", "r").readlines()]
     input = [open(PurePath(sys.argv[0]).with_suffix(".txt"), "r").readlines()]
-    
 
-    valid = process(input, list([ int(x) for x in list("11111111111111") ]))
+    valid = process(input, list([int(x) for x in list("11111111111111")]))
     print(f"{'valid' if valid == 0 else 'invalid'}")
