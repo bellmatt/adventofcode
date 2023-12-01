e7061acd5621bb6e59345a9c775b37d1fc210729
Matt Bell <38588937+bellmatt@users.noreply.github.com>
2023-12-01 10:02:30 +0000

    Day 1 part 2

--- 2023/day1.php
+++ 2023/day1.php
@@ -1,46 +1,67 @@
 <?php
 
 $example_array = explode("\n", file_get_contents("day1_example.txt"));
+$example_array_part2 = explode("\n", file_get_contents("day1_example_2.txt"));
 $array = explode("\n", file_get_contents("day1.txt"));
 
-echo calculate_calibration_values($example_array) . "\n";
-echo calculate_calibration_values($array) . "\n";
+echo calculate_calibration_values_ints($example_array) . "\n";
+echo calculate_calibration_values_ints($array) . "\n";
 
-
-function calculate_calibration_values($array) {
+function calculate_calibration_values_ints($array) {
     $total = 0;
     for ($i = 0; $i < count($array); $i++) {
         $inner_array = str_split($array[$i]);
-        # Find first digit
-        $first_digit = 0;
-        $last_digit = 0;
-        for ($j = 0; $j < count($inner_array); $j++) {
-            if ($inner_array[$j] != '0') {
-                $first_digit = intval($inner_array[$j]);
-                if ($first_digit > 0) {
-                    break;
-                }
-            }
-            else {
-                $first_digit = 0;
-                break;
-            }
-        }
-        # Find last digit
-        for ($j = array_key_last($inner_array); $j >= 0; $j--) {
-            if ($inner_array[$j] != '0') {
-                $last_digit = intval($inner_array[$j]);
-                if ($last_digit > 0) {
-                    break;
-                }
-            }
-            else {
-                $last_digit = 0;
-                break;
-            }
-        }
-        $total += intval($first_digit . $last_digit);
-        
+        $total += intval(get_first_digit($inner_array) . get_last_digit($inner_array));
     }
     return $total;
+}
+
+function get_first_digit($array) {
+    $numbers = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    $first_digit = 0;
+    for ($i = 0; $i < count($array); $i++) {
+        if ($array[$i] != '0') {
+            $first_digit = intval($array[$i]);
+            if ($first_digit > 0) {
+                return $first_digit;
+            }
+        }
+        else {
+            $first_digit = 0;
+            return $first_digit;
+        }
+        for ($n = 0; $n < count($numbers); $n++) {
+            //echo "Checking " . $numbers[$n] . " against " . implode(array_slice($array, $i, 5)) . "\n";
+            if (str_starts_with(implode(array_slice($array,$i)), $numbers[$n])) {
+                //echo "Found first digit: " . $numbers[$n] . "\n";
+                return $n + 1;
+            }
+        }
+    }
+    return $first_digit;
+}
+
+function get_last_digit($array) {
+    $numbers = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    $last_digit = 0;
+    for ($i = array_key_last($array); $i >= 0; $i--) {
+        if ($array[$i] != '0') {
+            $last_digit = intval($array[$i]);
+            if ($last_digit > 0) {
+                return $last_digit;
+            }
+        }
+        else {
+            $last_digit = 0;
+            return $last_digit;
+        }
+        for ($n = 0; $n < count($numbers); $n++) {
+            //echo "Checking " . $numbers[$n] . " against " . implode(array_slice($array,$i)) . "\n";
+            if (str_starts_with(implode(array_slice($array, $i)), $numbers[$n])) {
+                //echo "Found last digit: " . $numbers[$n] . "\n";
+                return $n + 1;
+            }
+        }
+    }
+    return $last_digit;
 }
