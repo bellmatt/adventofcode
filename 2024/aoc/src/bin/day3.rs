84be9e7dc8c46e13f34e06ddaa2666c8da225efb
Matt Bell <38588937+bellmatt@users.noreply.github.com>
2024-12-08 17:25:54 +0000

    Day 3 part 2

--- 2024/aoc/src/bin/day3.rs
+++ 2024/aoc/src/bin/day3.rs
@@ -1,5 +1,11 @@
 use regex::Regex;
 
+fn calculate_mul(instruction: &str) -> i32 {
+    let mut split = instruction.split(",");
+    let x = split.next().unwrap().replace("mul(", "").parse::<i32>().unwrap();
+    let y = split.next().unwrap().replace(")", "").parse::<i32>().unwrap();
+    return x * y
+}
 
 fn main() {
     let input = include_str!("../input/day3_example.txt");
@@ -8,11 +14,36 @@ fn main() {
     let matches: Vec<_> = re.find_iter(input).map(|m| m.as_str()).collect();
     let mut sum = 0;
     for instruction in matches {
-        let mut split = instruction.split(",");
-        let x = split.next().unwrap().replace("mul(", "").parse::<i32>().unwrap();
-        let y = split.next().unwrap().replace(")", "").parse::<i32>().unwrap();
-        sum += x * y;
+        calculate_mul(instruction);
     }
     println!("Sum: {}", sum);
     
+    //let input = include_str!("../input/day3_example2.txt");
+    let re_find_dont = Regex::new(r"don't\(\)").unwrap();
+    let re_find_do = Regex::new(r"do\(\)").unwrap();
+    let matches: Vec<&str> = re_find_dont.split(input).collect();
+    let mut sum_part2 = 0;
+    for (i, section) in matches.iter().enumerate() {
+        if i == 0 {
+            // Assume first one is enabled (unless the don't() is right at the start?)
+            let got: Vec<_> = re.find_iter(section).map(|m| m.as_str()).collect();
+            for instruction in got {
+                sum_part2 += calculate_mul(instruction);
+            }
+        }
+        else {
+            // re-run the regex but look for do() to re-enable
+            let got: Vec<&str> = re_find_do.split(section).collect();
+            for (i, section) in got.iter().enumerate() {
+                if i == 0 {
+                    continue;
+                }
+                let found: Vec<_> = re.find_iter(section).map(|m| m.as_str()).collect();
+                for instruction in found {
+                    sum_part2 += calculate_mul(instruction);
+                }
+            }
+        }
+    }
+    println!("Sum part 2: {}", sum_part2);
 }
