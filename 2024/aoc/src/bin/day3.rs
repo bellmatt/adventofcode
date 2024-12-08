cb629beccf7611a199b5448ca4bfea918cdae3c7
Matt Bell <38588937+bellmatt@users.noreply.github.com>
2024-12-08 08:20:30 +0000

    Day 3 part 1

--- 2024/aoc/src/bin/day3.rs
+++ 2024/aoc/src/bin/day3.rs
@@ -2,13 +2,17 @@ use regex::Regex;
 
 
 fn main() {
-    let input = include_str!("../input/day2_example.txt");
-    let input = include_str!("../input/day2_input.txt");
-
-    // Parse the input into a vector of vectors
-    let v: Vec<_> = input
-    .lines()
-    .map(|line| line.split(" ").collect::<Vec<_>>())
-    .collect::<Vec<_>>();
-
+    let input = include_str!("../input/day3_example.txt");
+    let input = include_str!("../input/day3_input.txt");
+    let re = Regex::new(r"mul\([0-9]*,[0-9]*\)").unwrap();
+    let matches: Vec<_> = re.find_iter(input).map(|m| m.as_str()).collect();
+    let mut sum = 0;
+    for instruction in matches {
+        let mut split = instruction.split(",");
+        let x = split.next().unwrap().replace("mul(", "").parse::<i32>().unwrap();
+        let y = split.next().unwrap().replace(")", "").parse::<i32>().unwrap();
+        sum += x * y;
+    }
+    println!("Sum: {}", sum);
+    
 }
